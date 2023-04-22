5cf050635456bf7d3bf353031e5e0500c98c7c26
ZacharyCallahan <zachcallahan90@gmail.com>
2023-04-22 16:30:34 -0400

    Next Environment

---
+++ client/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <h1>test</h1>
+        {children}</body>
+    </html>
+  )
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        "groovy-yellow": "#F8F933",
-        "groovy-blue": "#00D4E0",
-        "groovy-purple": "#BA61FA",
-        "groovy-orange": "#F89C5E",
-        "groovy-red": "#D0515C",
-        "groovy-red-dark": "#a33e47",
-        "groovy-green": "#28F780",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [],
 }

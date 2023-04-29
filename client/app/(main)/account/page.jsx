ccc62758704717faa4b1cd346721086f484f264e
Zachary <41547985+ZacharyCallahan@users.noreply.github.com>
2023-04-29 15:02:09 -0700

    Update page.jsx
    Removed white backdrop was duplicated

--- client/app/(main)/account/page.jsx
+++ client/app/(main)/account/page.jsx
@@ -2,8 +2,6 @@ import AccountForm from "../../components/layout/account/AccountForm";
 
 export default function Account() {
     return (
-        <div className="flex grow-[3] flex-col bg-white p-5 rounded-md shadow-md gap-5 h-fit">
             <AccountForm />
-        </div>
     );
 }

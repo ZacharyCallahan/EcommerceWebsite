bb0b532587cf87014c8364aab9445e7e19bfa0db
ZacharyCallahan <zachcallahan90@gmail.com>
2023-05-17 23:32:07 -0400

    test

--- client/app/loading.jsx
+++ client/app/loading.jsx
@@ -3,5 +3,6 @@ export default function Loading() {
         <div className="flex justify-center items-center h-screen">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-groovy-purple"></div>
         </div>
+        
     );
 }

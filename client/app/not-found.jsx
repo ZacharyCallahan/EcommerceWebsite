89f1f1ec64776941d30d99828ba8a8f96c9a0204
ZacharyCallahan <zachcallahan90@gmail.com>
2023-05-19 19:18:04 -0400

    Updated not-found

--- client/app/not-found.jsx
+++ client/app/not-found.jsx
@@ -1,21 +1,17 @@
 import Link from "next/link";
-import "./globals.css"
-
+import "./globals.css";
+import Button from "./components/ui/Button";
 
 const notfound = () => {
     return (
-        <div className="flex flex-col justify-center items-center h-screen gap-5 text-gray-100">
-            <h1 className="text-9xl font-bold text-groovy-red">404</h1>
-            <h2 className="text-4xl font-bold text-groovy-red">
-                Sorry, the page you are looking for cannot be found or is under
-                construction.
-            </h2>
-
-            <Link
-                href={"/"}
-                className="p-4 bg-groovy-red rounded-md shadow-md text-white font-bold text-xl">
-                Go back home
-            </Link>
+        <div className="flex flex-col justify-center items-center h-screen text-gray-100">
+            <div className="w-1/2 text-center space-y-5">
+                <h1 className="text-9xl font-bold text-groovy-red">404</h1>
+                <h2 className="text-4xl font-bold text-groovy-red">
+                    Sorry, the page you are looking for cannot be found or is
+                    under construction.
+                </h2>
+            </div>
         </div>
     );
 };

334ed2a4ba67a783d720f386cf607a6f174fbd05
ZacharyCallahan <zachcallahan90@gmail.com>
2023-05-27 16:18:30 -0400

    Temp fix for confirm route

--- client/app/components/ui/checkout/CheckoutForm.jsx
+++ client/app/components/ui/checkout/CheckoutForm.jsx
@@ -74,7 +74,7 @@ const CheckoutForm = () => {
             //`Elements` instance that was used to create the Payment Element
             elements,
             confirmParams: {
-                return_url: `http://localhost:3000/`,
+                return_url: `https://groovygear.vercel.app/`,
             },
         });
 
